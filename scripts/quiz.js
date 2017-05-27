97111571a5cc03bd5e9b2bfbdfa2e91e72182407
DB987 <davidbrown21123@gmail.com>
2017-05-27 17:28:09 +0100

    Add files via upload

---
+++ scripts/quiz.js
@@ -0,0 +1,87 @@
+var questions = [
+  ["What do you add to test for transition metals?", "NaOH"],
+  ["What colour ppt will Cu2+ form?", "blue"],
+  ["What colour ppt will Fe3+ form?", "orange"],
+  ["What colour ppt will Fe2+ form?", "green"],
+  ["What colour ppt will Cr3+ form?", "grey-green"],
+  ["What colour ppt will Mn2+ form?", "pink"],
+  ["What do you add to test for halides?", "nitric acid then silver nitrate"],
+  ["What do you add to test for carbonates?", "dilute acid"],
+  ["What is a postive result for carbonates?", "effervescence and limewater turning cloudy"],
+  ["What do you add to test for sulfates?", "dilute strong acid then a few drops of barium nitrate solution"],
+  ["What is a postive result for sulfates?", "white ppt"],
+  ["What do you add to test for ammonium?", "add a few drops of sodium hydroxide and warm"],
+];
+var number = 0;
+var question = document.getElementById('question');
+var textbox = document.getElementById('answer');
+var submit = document.getElementById('submit');
+var next = document.getElementById('next');
+var correct = document.getElementById('correct');
+var back = document.getElementById('back');
+var restart = document.getElementById('restart')
+var colour = "";
+var length = questions.length;
+var right = 0;
+var entercheck = 0;
+next.style.display = "none";
+back.style.display = "none";
+restart.style.display = "none";
+question.innerHTML = questions[number][0];
+function submitFunction() {
+  entercheck = 1;
+  var x = textbox.value;
+  if (x === questions[number][1]) {
+    correct.innerHTML = "Correct";
+    correct.style.color = "green"
+    right = right + 1
+  } else {
+    correct.innerHTML = "Incorrect. The answer is " + questions[number][1];
+    correct.style.color = "red";
+  }
+  submit.style.display = 'none';
+  next.style.display = "block";
+  number = number + 1;
+}
+function nextFunction() {
+  entercheck = 0
+  if (length === number) {
+    results = "Well done you have finished. You achieved " + right + "/" + length + ".";
+    question.style.display = "none";
+    textbox.style.display = "none";
+    submit.style.display = "none";
+    next.style.display = "none";
+    correct.style.color = "black";
+    correct.innerHTML = results;
+    back.style.display = "block";
+    restart.style.display = "block";
+  } else {
+    question.innerHTML = questions[number][0];
+    textbox.value = "";
+    correct.innerHTML = "";
+    submit.style.display = "block";
+    next.style.display = "none";
+  }
+}
+function backFunction() {
+  location.href='chemistry.html';
+}
+function restartFunction() {
+  number = 0;
+  next.style.display = "none";
+  back.style.display = "none";
+  restart.style.display = "none";
+  textbox.style.display = "block";
+  submit.style.display = "block";
+  correct.innerHTML = "";
+  question.style.display = "block";
+  question.innerHTML = questions[number][0];
+}
+textbox.addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode == 13 && entercheck === 0) {
+      submitFunction();
+    } else if (event.keyCode == 13 && entercheck === 1) {
+      nextFunction();
+    }
+});
---
+++ scripts/subjects.js
@@ -0,0 +1,11 @@
+$('#b1').click(function(){
+    location.href='index.html';
+})
+
+$('#bp1').click(function(){
+    location.href='interactions.html';
+})
+
+$('#bc1').click(function(){
+    location.href='test_for_ions.html';
+})
